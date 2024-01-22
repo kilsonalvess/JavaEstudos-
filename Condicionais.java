5f2ca58631bd9d9f5c483a4859c277f74bcd5d1b
Kilson <kilsonalves@hotmail.com>
2024-01-22 12:13:17 -0300

    Condionais

---
+++ Condicionais.java
@@ -0,0 +1,21 @@
+public class Condicionais {
+    public static void main(String[] args) {
+        //*Expressões comparativas
+        //* < menor
+        //* > maior
+        //* == igual
+        //* <= menor igual
+        //* >= maior igual
+        //!= diferente
+        //*&& e todas as condições tem que ser verdadeiras
+        //* || ou basta uma ser verdadeira para o resultado ser verdadeiro
+        //* ! não
+
+
+
+
+
+
+
+    }
+}
--- Main.java
+++ Main.java
@@ -1,7 +1,36 @@
 import org.w3c.dom.ls.LSOutput;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+
+
+
+        Scanner leitura = new Scanner(System.in);
+        System.out.println("Digite o numero de funcionarios: ");
+
+        int funcionarios  = leitura.nextInt();
+
+        System.out.println("Digite o valor: ");
+        int calculo = leitura.nextInt();
+
+        System.out.println("Digite o numero de hora: ");
+        double hora = leitura.nextDouble();
+
+
+        System.out.println("Numero de funcionarios: "+funcionarios);
+        System.out.println("Salario: "+calculo * hora);
+
+
+
+
+
+
+
+
+
+
+
 
 
 
